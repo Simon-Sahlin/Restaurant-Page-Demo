841f8c891b86b6536d60189c60b1bfb149d5a22e
Simon-Sahlin <simonsahlin@somin.se>
2024-04-09 16:04:18 +0200

    Add Tab Switching Functionality With Supporting Styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import './style.css'
 import homepage from "./homepage.js"
 
-let contentWrapper = document.querySelector("#content");
 
-contentWrapper.appendChild(homepage());
+let tabHeaders = document.querySelectorAll("nav>button");
+tabHeaders.forEach((btn, i) => {
+    btn.addEventListener("click", ()=>changeTab(i));
+});
+
+let contentWrapper = document.querySelector("#content");
+let pages = [
+    homepage(),
+    document.createElement("div"),
+    document.createElement("div"),
+];
+
+function changeTab(index){
+    tabHeaders.forEach(btn => {
+        btn.classList.remove("selected");
+    });
+    tabHeaders[index].classList.add("selected");
+    contentWrapper.innerHTML = "";
+    contentWrapper.appendChild(pages[index]);
+}
+
+changeTab(0);
